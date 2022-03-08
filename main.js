9fe7f7d0682620a6362ba379e3a5596f27bc0669
Tiago Pereira Dantas <devdantas@pop-os.localdomain>
2022-03-08 01:46:50 -0300

    help da comunidade

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
+const BASEURL = "http://localhost:3333/api";
 
-async function addNewUsers() {
-  axios.post(process.env.BASEURL)
+async function addNewToSend() {
+  const form = document.querySelector('form');
+
+  const formData = {
+    grupo: form.grupo.value,
+    razao_social: form.razao_social.value,
+    nome_fantasia: form.nome_fantasia.value,
+    cpf_cnpj: form.cpf_cnpj.value,
+    inscricao_estadual: form.inscricao_estadual.value,
+    data_nascimento: form.data_nascimento.value,
+    endereco: form.endereco.value,
+    numero: form.numero.value,
+    complemento: form.complemento.value,
+    bairro: form.bairro.value,
+    cep: form.cep.value,
+    cidade: form.cidade.value,
+    uf: form.uf.value,
+    telefone: form.telefone.value,
+    telefone_celular: form.telefone_celular.value,
+    email: form.email.value,
+    email_nfe: form.email_nfe.value,	
+  }
+  axios.post(BASEURL, formData)
+    .then(response => console.log(response.data))
+    .catch(error => error)
 }
